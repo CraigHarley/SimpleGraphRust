79eee4b6321cf90add69e71ebcbf6746716197be
Craig Harley <craigharley2@googlemail.com>
2019-02-17 01:52:42 +0000

    Tiny cleanup

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,12 @@ use std::collections::HashMap;
 
 fn main() {
     let matrix = HashMap::new();
-    let step1 = add_edge(matrix, 1, 2);
-    let step2 = add_edge(step1, 5, 1);
-    let step3 = add_edge(step2, 5, 2);
-    let step4 = add_edge(step3, 2, 6);
+    let matrix = add_edge(matrix, 1, 2);
+    let matrix = add_edge(matrix, 5, 1);
+    let matrix = add_edge(matrix, 5, 2);
+    let matrix = add_edge(matrix, 2, 6);
 
-    for (&i, second_level) in step4.iter() {
+    for (&i, second_level) in matrix.iter() {
         for (&j, &value) in second_level.iter() {
             println!("Calling {}, {}: {}", i, j, value);
         }
