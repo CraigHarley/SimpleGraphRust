d8f2d12daf9290cde3f303235e2af616a0246efb
Craig Harley <craigharley2@googlemail.com>
2019-02-17 15:05:39 +0000

    Now gets data from sql into the matrix

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,14 @@ extern crate mysql;
 use std::collections::HashMap;
 use mysql::QueryResult;
 
-fn main() {
-    insert_edges_from_mysql();
+struct PlayerLink {
+    id: u32,
+    fromPlayerId: u32,
+    toPlayerId: u32,
+}
 
-    let matrix = create_graph();
+fn main() {
+    let matrix = create_graph_from_mysql();
 
     for (&i, second_level) in matrix.iter() {
         for (&j, &value) in second_level.iter() {
@@ -15,12 +19,29 @@ fn main() {
     }
 }
 
-fn create_graph() -> HashMap<u32, HashMap<u32, bool>> {
-    let matrix = HashMap::new();
-    let matrix = add_edge(matrix, 1, 2);
-    let matrix = add_edge(matrix, 5, 1);
-    let matrix = add_edge(matrix, 5, 2);
-    let matrix = add_edge(matrix, 2, 6);
+fn create_graph_from_mysql() -> HashMap<u32, HashMap<u32, bool>> {
+    let pool = mysql::Pool::new("mysql://root@localhost:3306/mysql").unwrap();
+    let mut matrix = HashMap::new();
+
+    let player_links: Vec<PlayerLink> =
+        pool.prep_exec("SELECT id, fromPlayerId, toPlayerId FROM sixdegrees.player_links", ())
+            .map(|result: QueryResult| {
+                result
+                    .map(|x| x.unwrap())
+                    .map(|row| {
+                        let (id, fromPlayerId, toPlayerId) = mysql::from_row(row);
+                        PlayerLink {
+                            id,
+                            fromPlayerId,
+                            toPlayerId,
+                        }
+                    }).collect()
+            }).unwrap();
+
+    for link in player_links {
+        matrix = add_edge(matrix, link.fromPlayerId, link.toPlayerId);
+    }
+
     return matrix;
 }
 
@@ -32,34 +53,4 @@ fn add_edge(
     matrix.entry(i).or_insert_with(HashMap::new).insert(j, true);
     matrix.entry(j).or_insert_with(HashMap::new).insert(i, true);
     return matrix;
-}
-
-
-struct PlayerLink {
-    id: u32,
-    fromPlayerId: u32,
-    toPlayerId: u32,
-}
-
-fn insert_edges_from_mysql() {
-    let pool = mysql::Pool::new("mysql://root@localhost:3306/mysql").unwrap();
-
-    let player_links: Vec<PlayerLink> =
-        pool.prep_exec("SELECT id, fromPlayerId, toPlayerId FROM sixdegrees.player_links LIMIT 100", ())
-            .map(|result: QueryResult| {
-                result.map(|x|
-                    x.unwrap())
-                    .map(|row| {
-                        let (id, fromPlayerId, toPlayerId) = mysql::from_row(row);
-                        PlayerLink {
-                            id,
-                            fromPlayerId,
-                            toPlayerId,
-                        }
-                    }).collect()
-            }).unwrap();
-
-    for x in player_links {
-        println!("Links: {}, {}: {}", x.id, x.fromPlayerId, x.toPlayerId);
-    }
 }
