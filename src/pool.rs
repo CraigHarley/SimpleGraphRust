828ed1efaca6713a63da7ef6c5e68b9157d356ee
Craig Harley <craigharley2@googlemail.com>
2019-04-22 19:30:37 +0100

    format

--- src/pool.rs
+++ src/pool.rs
@@ -2,11 +2,11 @@ use mysql::Pool;
 
 pub fn get_pool() -> Pool {
     let mut builder = mysql::OptsBuilder::new();
-    builder.ip_or_hostname(Some("127.0.0.1"))
+    builder
+        .ip_or_hostname(Some("127.0.0.1"))
         .db_name(Some("sixdegrees"))
         .user(Some("root"))
         .pass(Some(""));
-
 
     mysql::Pool::new(builder).unwrap()
 }
